d3cd0457879fc2c437e953d740eb1392e6bf04f4
kmaherx <85698689+kmaherx@users.noreply.github.com>
2025-05-16 19:47:27 +0000

    Deploying to gh-pages from @ kmaherx/kmaherx.github.io@c35c33fad4ad45749392a7b3dda9b6b890fd3279 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -114,6 +114,13 @@ ninja.data = [{
           window.open("mailto:%6B%61%6D%61%6C.%6D.%6D%61%68%65%72@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/kmaherx", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
